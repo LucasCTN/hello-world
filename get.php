51542df9aa4606df5e261a5246aead5fdf567a52
Lucas <luk3@hotmail.com.br>
2015-05-08 19:45:28 -0300

    GET request in PHP cURL

---
+++ get.php
@@ -0,0 +1,22 @@
+<?php
+
+	$curl = curl_init();
+
+	//Set request link to get info in a subreddit.
+	curl_setopt($cURL, CURLOPT_URL, 'API_LINK_HERE');
+
+	//Setting headers.
+	curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'Voat-ApiKey: API_PUBLIC_KEY_HERE',
+	));
+
+	// Send the request & save response to $resp
+	$resp = curl_exec($curl);
+
+	// Close request to clear up resources
+	curl_close($curl);
+
+	// Echo request to get the info (optional)
+	echo ($resp);
+?> 
